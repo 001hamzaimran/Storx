01a4bff5d01c0f1ce5b65dfcf70b35bef2d941ea
001hamzaimran <syed.hamza2004@gmail.com>
2025-03-25 17:05:12 +0500

    make it better

--- web/utils/db.js
+++ web/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/Storx");
+    await mongoose.connect("mongodb+srv://zahid:zahid313@cluster0.7kqkz.mongodb.net/StoreX");
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
